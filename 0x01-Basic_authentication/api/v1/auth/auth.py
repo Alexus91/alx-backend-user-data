11c28857772e335b9d93b042b74173e2856dd3a5
Alexus91 <mrfadlkamal@gmail.com>
2024-02-13 02:56:01 +0100

    try new aspect to fix output

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,17 +15,30 @@ class Auth:
         Returns:
         - bool: True if authentication is required, False otherwise.
         """
-        if not path or not excluded_paths:
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
-        if path in excluded_paths:
-            return False
+        l_path = len(path)
+        if l_path == 0:
+            return True
+
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
 
         for exc in excluded_paths:
-            if exc.endswith('*') and path.startswith(exc[:-1]):
-                return False
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
 
-        return True
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
 
     def authorization_header(self, request=None) -> str:
         """
